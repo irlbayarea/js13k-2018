837f4f4426b7379fdd2fb409db8188d8c46b07a6
Matan Lurey <matan@lurey.org>
2018-08-05 23:44:15 -0700

    Add auto-zip.

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,9 +2,16 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 const TerserPlugin = require('terser-webpack-plugin');
+const ZipPlugin = require('zip-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new ZipPlugin({
+        filename: 'release.zip',
+        pathPrefix: 'dist'
+    })
+  ],
   optimization: {
       minimizer: [
         new TerserPlugin({
