b8c00479e84bf11d5471c7650ba6537e4eb6d74b
Matan Lurey <matanlurey@users.noreply.github.com>
2018-08-06 18:07:36 -0700

    Add some optimizations. (#6)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 class DocumentBodyNamer {
   public name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
 }
 
-const namer = new DocumentBodyNamer();
-namer.name = 'Hello World!';
+const namer = new DocumentBodyNamer('Hello World!');
 document.body.innerHTML = namer.name;
