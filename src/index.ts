9127711dd822a04cfbe1fe50fbdc38a2cb1bdab5
Matan Lurey <matan@lurey.org>
2018-08-18 14:06:10 -0700

    Full screen mobile canvas w/ 'Hello'.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-import { drawToCanvas, writeToParagraph, writeWordsOnCanvas } from './draw';
+const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+const context = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-writeToParagraph(document, `HelloWorld`, `Hello there!`);
-drawToCanvas(document, `tutorial`);
-writeWordsOnCanvas(document, `tutorial2`, `HAANDS`);
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.clientWidth;
+
+const centerRatio = 2;
+
+function renderFrames(time: number): void {
+  canvas.width = canvas.width;
+  context.textAlign = 'center';
+  context.fillText(
+    `Hello There! ${Math.round(time)}`,
+    canvas.width / centerRatio,
+    canvas.height / centerRatio
+  );
+  window.requestAnimationFrame(renderFrames);
+}
+
+window.requestAnimationFrame(renderFrames);
