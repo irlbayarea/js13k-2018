ffae8377291d27834c790c0a57fdb752a41a5a52
Matan Lurey <matan@lurey.org>
2018-08-19 19:25:02 -0700

    Fmt.

--- src/common/flags.d.ts
+++ src/common/flags.d.ts
@@ -1,6 +1,6 @@
 declare const FLAGS_DIMENSIONS: {
-  width: number,
-  height: number,
+  width: number;
+  height: number;
 };
 declare const FLAGS_PRODUCTION: boolean;
 declare const FLAGS_VERSION: string;
