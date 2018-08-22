bf1418736e9090b6c1878e1cf1a2beea48fe2113
Matan Lurey <matan@lurey.org>
2018-08-22 16:13:34 -0600

    Format.

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,38 +5,39 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const ZipPlugin = require('zip-webpack-plugin');
 
-module.exports = env => merge(common, {
-  mode: 'production',
-  plugins: [
-    new ZipPlugin({
-      filename: 'release.zip',
-      pathPrefix: 'dist',
-    }),
-  ],
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          ecma: 6,
-          compress: {
-            arguments: true,
-            hoist_funs: true,
-            keep_fargs: false,
-            module: true,
-            passes: 10,
-            pure_getters: true,
-            toplevel: true,
-            unsafe: true,
-          },
-          mangle: {
-            toplevel: true,
-          },
-          output: {
-            beautify: env && env.beautify,
-            comments: false,
-          },
-        },
+module.exports = env =>
+  merge(common, {
+    mode: 'production',
+    plugins: [
+      new ZipPlugin({
+        filename: 'release.zip',
+        pathPrefix: 'dist',
       }),
     ],
-  },
-});
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            ecma: 6,
+            compress: {
+              arguments: true,
+              hoist_funs: true,
+              keep_fargs: false,
+              module: true,
+              passes: 10,
+              pure_getters: true,
+              toplevel: true,
+              unsafe: true,
+            },
+            mangle: {
+              toplevel: true,
+            },
+            output: {
+              beautify: env && env.beautify,
+              comments: false,
+            },
+          },
+        }),
+      ],
+    },
+  });
