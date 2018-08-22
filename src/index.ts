17d867f147f15f9b86592a1ace1a180a1d2c63fa
Alexander J. Kindel <alexkindel@gmail.com>
2018-08-22 00:14:41 -0700

    ak: Adding time / time delta parameters to update.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
 import { World } from './game/world/world';
 import { Draw } from './graphics/draw';
-// const context = canvas.getContext('2d') as CanvasRenderingContext2D;
-
-// function readCanvasDimensions(): void {
-//   canvas.width = FLAGS_DIMENSIONS.width;
-//   canvas.height = FLAGS_DIMENSIONS.height;
-// }
-
-// readCanvasDimensions();
 
 class State {
   public readonly world: World = new World();
@@ -24,7 +16,7 @@ function renderFrames(time: number): void {
   state.draw.context.textAlign = 'center';
   state.draw.text(`Hello There! ${Math.round(time)}`, state.draw.getCenter());
 
-  state.world.update();
+  state.world.update(time);
   state.world.render();
 
   window.requestAnimationFrame(renderFrames);
