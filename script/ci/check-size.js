2a90bb591256f605dd4243e49b0ec59efe6bc3da
QuantumBits <classicrockjunkie@gmail.com>
2018-08-31 21:00:16 -0700

    Added % to build script output

--- script/ci/check-size.js
+++ script/ci/check-size.js
@@ -3,7 +3,9 @@ const stats = fs.statSync('dist/release.zip');
 const fileSizeInBytes = stats.size;
 const maxFileSize = 1024 * 13;
 
-const message = `File size of "dist/release.zip" is ${fileSizeInBytes} bytes`;
+const message = `File size of "dist/release.zip" is ${fileSizeInBytes}/${maxFileSize} bytes (${(100 *
+  fileSizeInBytes) /
+  maxFileSize}) %`;
 if (fileSizeInBytes > 1024 * 13) {
   console.error(message);
   process.exit(1);
