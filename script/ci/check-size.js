0a113c7ac32cddc53e8fc7ac6b1aa450c733b901
QuantumBits <classicrockjunkie@gmail.com>
2018-08-31 21:07:42 -0700

    Added % to build script output (#48)
    * Added % to build script output
    
    * Removing magic numbers

--- script/ci/check-size.js
+++ script/ci/check-size.js
@@ -3,8 +3,10 @@ const stats = fs.statSync('dist/release.zip');
 const fileSizeInBytes = stats.size;
 const maxFileSize = 1024 * 13;
 
-const message = `File size of "dist/release.zip" is ${fileSizeInBytes} bytes`;
-if (fileSizeInBytes > 1024 * 13) {
+const message = `File size of "dist/release.zip" is ${fileSizeInBytes}/${maxFileSize} bytes (${(100 *
+  fileSizeInBytes) /
+  maxFileSize}) %`;
+if (fileSizeInBytes > maxFileSize) {
   console.error(message);
   process.exit(1);
 } else {
